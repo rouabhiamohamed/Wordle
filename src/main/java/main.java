e8560dfe8b7ec97f5c5b4587f253515ec35b1a80
Nassim Ezzaamari <hehe@live.fr>
2024-01-08 04:44:27 +0100

    Finally, javafx properly added to the project >_>

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,8 @@
 import backend.Partie;
 
+// --module-path "./javafx/lib" --add-modules javafx.controls,javafx.fxml
+// Dans run / edit configuration / edit vm truc
+
 public class main {
     public static void main(String [] args){
 
