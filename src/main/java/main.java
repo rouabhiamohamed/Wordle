8a26dfb7520eea6ba6a15557b717abafa5b827ff
Nassim Ezzaamari <hehe@live.fr>
2024-01-08 05:00:12 +0100

    Added the user interface in the main as well, tomorrow will be a loooooooong day, anyhow good night

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,6 @@
 import backend.Hint;
 import backend.Partie;
+import frontend.wordleMain;
 
 // --module-path "./javafx/lib" --add-modules javafx.controls,javafx.fxml
 // Dans run / edit configuration / edit vm truc
@@ -11,6 +12,11 @@ public class main {
         //Don't forget to boot the server though if you're expecting an answer lol
         System.out.println(Hint.getOneHint("arbre"));
 
+        //How to boot the user interface
+        //Somehow this shit works lmao
+        wordleMain.main(args);
+
+        //How to use the backend
         Partie game = new Partie();
         game.jouer();
 
